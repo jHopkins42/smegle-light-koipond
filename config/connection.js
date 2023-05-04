01de755980d050659071ead980791b6725a2ce70
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-04 00:16:59 -0400

    updating config

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
