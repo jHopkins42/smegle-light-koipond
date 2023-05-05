4de4fc6dc454100ff0f7a57aae8467d6b1f9ae93
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-05 00:22:38 -0400

    keyboard cat update

--- models/index.js
+++ models/index.js
@@ -3,13 +3,21 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { HasMany } = require('sequelize');
 
 // Products belongsTo Category
-
+Product.hasOne(Category, {
+  foreignKey:,
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
-
+Category:HasMany(Product, {
+  //cat on the keyboard
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
 
+});
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
